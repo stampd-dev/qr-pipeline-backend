5b1a7457d6421ef1476e45d321c2a3e230ed774d
sstewart-eng <sstewart@under.io>
2025-11-12 12:26:57 -0600

    made recent ripple unique by code so we see more variance

--- src/queries/get-most-recent-ripples.ts
+++ src/queries/get-most-recent-ripples.ts
@@ -4,6 +4,7 @@ import {
   QueryCommandInput,
 } from "@aws-sdk/lib-dynamodb";
 import { RippleEvent } from "../types/dynamo";
+import { getReferrerName } from "../handlers/get-top-codes/utils";
 
 export const getMostRecentRipples = async ({
   client,
@@ -21,7 +22,7 @@ export const getMostRecentRipples = async ({
     ExpressionAttributeValues: {
       ":pk": "RIPPLE",
     },
-    Limit: 5,
+    Limit: 100,
     ScanIndexForward: false,
   };
   const command = new QueryCommand(commandInput);
@@ -38,5 +39,26 @@ export const getMostRecentRipples = async ({
   const response = await client.send(command);
   console.log("[GetMostRecentRipples] Response", response);
 
-  return response.Items as RippleEvent[];
+  const unformattedResults = response.Items as RippleEvent[];
+
+  /** Create a unique set from the ripples from the item property code */
+  const uniqueCodes: { location: string; referrer: string; code: string }[] =
+    [];
+  unformattedResults.forEach((ripple) => {
+    if (!uniqueCodes.some((code) => code.code === ripple.code)) {
+      uniqueCodes.push({
+        location: ripple.location,
+        referrer: ripple.referrer,
+        code: ripple.code,
+      });
+    }
+  });
+
+  const formattedResults = uniqueCodes.map((code) => ({
+    location: code.location,
+    referrer: getReferrerName(code.referrer),
+    code: code.code,
+  }));
+
+  return formattedResults;
 };
